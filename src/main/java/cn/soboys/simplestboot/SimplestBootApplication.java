ce688cb6719e03e6fb8cf39a61ec6f62498d1f22
kenx <17521197955@163.com>
2023-08-01 16:17:25 +0800

    init

---
+++ src/main/java/cn/soboys/simplestboot/SimplestBootApplication.java
@@ -0,0 +1,13 @@
+package cn.soboys.simplestboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplestBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplestBootApplication.class, args);
+    }
+
+}
