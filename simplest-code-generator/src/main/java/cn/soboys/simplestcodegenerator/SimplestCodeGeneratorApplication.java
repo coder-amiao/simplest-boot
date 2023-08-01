fe7feb36c8c8a596392bae8595a6024af011e147
kenx <17521197955@163.com>
2023-08-01 17:42:27 +0800

    coder-generator

---
+++ simplest-code-generator/src/main/java/cn/soboys/simplestcodegenerator/SimplestCodeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package cn.soboys.simplestcodegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplestCodeGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplestCodeGeneratorApplication.class, args);
+    }
+
+}
---
+++ simplest-code-generator/src/test/java/cn/soboys/simplestcodegenerator/SimplestCodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.soboys.simplestcodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplestCodeGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
