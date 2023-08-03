8122c6601712059df0afa2718264bb64dbddf339
kenx <17521197955@163.com>
2023-08-03 15:57:02 +0800

    接口更改

---
+++ simplest-core/src/main/java/cn/soboys/simplest/core/App.java
@@ -0,0 +1,13 @@
+package cn.soboys.simplest.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
